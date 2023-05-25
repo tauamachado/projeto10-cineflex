3df1c24937d868636d340d0829534da6c175bd96
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-05-25 20:30:58 -0300

    Implementação: Layout Tela 1 - HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,56 @@
-import styled from "styled-components"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import MovieContainer from "./MovieContainer";
+
+
 
 export default function HomePage() {
-    return (
-        <PageContainer>
-            Selecione o filme
+  const [movies, setMovies] = useState([]);
 
-            <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(
+        "https://mock-api.driven.com.br/api/v8/cineflex/movies"
+      );
+      setMovies(response.data);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-            </ListContainer>
-
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      <p>Selecione o filme</p>
+      <ListContainer>
+        {movies.map((movie) => (
+          <MovieContainer key={movie.id} image={movie.posterURL} />
+        ))}
+      </ListContainer>
+    </PageContainer>
+  );
 }
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-top: 70px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  text-align: center;
+  color: #293845;
+  margin-top: 30px;
+  padding-top: 70px;
+`;
+
 const ListContainer = styled.div`
-    width: 330px;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    padding: 10px;
-`
-const MovieContainer = styled.div`
-    width: 145px;
-    height: 210px;
-    box-shadow: 0px 2px 4px 2px #0000001A;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    img {
-        width: 130px;
-        height: 190px;
-    }
-`
+  width: 330px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  padding: 10px;
+`;
